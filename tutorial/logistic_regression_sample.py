ad87d4dfab8e2f305c7b608de41bd9a5727e268e
Zhan <xzhan99@hotmail.com>
2018-09-20 19:31:30 +1000

    new gradient descent function

--- tutorial/logistic_regression_sample.py
+++ tutorial/logistic_regression_sample.py
@@ -5,8 +5,8 @@ from scipy.optimize import minimize
 
 
 def read_file():
-    # folder_path = '/Users/andrewzhan/Downloads/'
-    folder_path = 'C:\\Users\\18701\\Desktop\\machine learning\\'
+    folder_path = '/Users/andrewzhan/Downloads/'
+    # folder_path = 'C:\\Users\\18701\\Desktop\\machine learning\\'
     with h5py.File(folder_path + 'images_training.h5', 'r') as H:
         train_set_x_orig = np.copy(H['data'])
         train_set_x_orig = train_set_x_orig / 255.
@@ -27,69 +27,50 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 
-def lost(w, X, y, learning_rate):
-    m = X.shape[1]
-    z = np.dot(X, w.T)
+def lost(theta, x, y):
+    m = x.shape[1]
+    z = np.dot(x, theta.T)
     exp_z = sigmoid(z)
+
     the_lost = np.sum(np.multiply(y, np.log(exp_z)) + np.multiply(1 - y, np.log(1 - exp_z))) / (-m)
     the_lost = np.squeeze(the_lost)
-
     # print(the_cost)
+
     if np.isnan(the_lost):
         return np.inf
     return the_lost
 
 
-def gradient(theta, X, y, learningRate):
+def gradient_descent(theta, x, y):
     theta = np.matrix(theta)
-    X = np.matrix(X)
-    print(X)
-    y = np.matrix(y)
-    m = len(X)
+    x = np.matrix(x)    # (n, 785)
+    y = np.matrix(y)    # (n, 1)
 
-    # parameters = int(theta.ravel().shape[1])
-    error = sigmoid(X * theta.T) - y
+    z = np.dot(x, theta.T)  #()
+    exp_z = sigmoid(z)  # (n, 1)
 
-    grad = ((X.T * error) / m).T + ((learningRate / m) * theta)
-    print(grad.shape)
+    dloss = np.sum(np.multiply((exp_z - y), x), axis=0)    # (1, 785)
 
-    # intercept gradient is not regularized
-    print(X[:, 0])
-    grad[0, 0] = np.sum(np.multiply(error, X[:, 0])) / m
-    print(grad.shape)
-    return np.array(grad).ravel()
+    return dloss.ravel()
 
 
-def logistic_loss_and_grad(theta, X, y):
-    theta = np.matrix(theta)
-    X = np.matrix(X)
-    y = np.matrix(y)
-
-    temp = -lost(theta, X, y) + y
-    print('temp: ', temp.shape)
-    dloss = -np.sum(np.multiply(temp, X), axis=0)
-    return dloss
-
-
-def one_vs_all(X, y, num_labels, learning_rate):
-    rows = X.shape[0]  # X的行数
-    params = X.shape[1]  # X的列数
+def one_vs_all(x, y, num_labels):
+    num_of_data = x.shape[0]
+    num_of_param = x.shape[1]
 
     # 对于k个分类器，构建k*（n+1）维向量组 (10, 785)
-    all_theta = np.zeros((num_labels, params + 1))
+    all_theta = np.random.random((num_labels, num_of_param + 1))
 
     # 在X第一列之前插入一列全为1的列向量作为常数项
-    X = np.insert(X, 0, values=np.ones(rows), axis=1)
+    x = np.insert(x, 0, values=np.ones(num_of_data), axis=1)
 
     # 对于y若将某个类别i拿出来之后剩下的类别构成一类
     for i in range(0, num_labels):
-        theta = np.zeros(params + 1)
+        theta = np.zeros(num_of_param + 1)
         y_i = np.array([1 if label == i else 0 for label in y])
-        y_i = np.reshape(y_i, (rows, 1))
+        y_i = np.reshape(y_i, (num_of_data, 1))
 
-        # 采用梯度下降法最小化目标函数（cost）
-        # fmin = minimize(fun=lost, x0=theta, args=(X, y_i), method='TNC', jac=logistic_loss_and_grad)
-        fmin = minimize(fun=lost, x0=theta, args=(X, y_i, learning_rate), method='TNC', jac=gradient)
+        fmin = minimize(fun=lost, x0=theta, args=(x, y_i), method='TNC', jac=gradient_descent)
         all_theta[i, :] = fmin.x
 
     return all_theta
@@ -123,9 +104,7 @@ if __name__ == "__main__":
     print(test_set_x.shape)
     print(test_set_y.shape)
 
-    data = {'X': train_set_x, 'y': train_set_y}
-
-    all_theta = one_vs_all(data['X'], data['y'], 10, 0.000001)
+    all_theta = one_vs_all(train_set_x, train_set_y, 10)
 
     y_pred = predict_all(test_set_x, all_theta)
     # for a in zip(y_pred, test_set_y):
